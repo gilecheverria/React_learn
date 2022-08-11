2ee9c4118eb859b046ec8b7bdf1faf16cb0f8114
Gilberto Echeverria <gilbertoef@gmail.com>
2022-08-11 17:54:51 -0500

    WIP: creating a dynamic list of clocks

--- clock-tutorial/src/index.js
+++ clock-tutorial/src/index.js
@@ -23,5 +23,81 @@ function MapApp() {
   );
 }
 
+class StartForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numClocks: 0,
+      color: Array(0),
+    }
+
+    // Bind the value of 'this'
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    if(target.type == "number") {
+      const colors = this.state.color.slice(0, value);
+      this.setState({numClocks: value, color: colors});
+    } else if(target.type == "color") {
+      const colors = this.state.color.slice();
+      colors[target.name] = value;
+      this.setState({color: colors});
+    }
+  }
+
+  handleSubmit(event) {
+    const numClocks = this.state.numClocks;
+    alert('Number of clocks: ' + numClocks);
+    event.preventDefault();
+  }
+
+  makeColorList() {
+    // Create a list of indices for the colors, starting at 1
+    const indices = Array.from({length: this.state.numClocks}, (v, k) => k + 1);
+    // https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
+    //const indices = Array.from(Array(this.state.numClocks).keys());
+    //const indices = [...Array(this.state.numClocks).keys()];
+    const colorFields = indices.map((i) =>
+      <div key={i} >
+        <label>
+          Color {i}:
+          <input
+            name={i}
+            type="color"
+            value={this.state.color[i]}
+            onChange={this.handleChange} />
+        </label>
+      </div>
+    );
+    return colorFields;
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Number of clocks:
+          <input
+            name="numClocks"
+            type="number"
+            value={this.state.numClocks}
+            onChange={this.handleChange} />
+        </label>
+        <label>
+          {this.makeColorList()}
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <MapApp /> );
+root.render( <StartForm /> );
+//root.render( <MapApp /> );
