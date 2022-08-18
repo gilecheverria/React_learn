ff204af74adc8280e9d6e0848cb9c5d4c13776e8
Gilberto Echeverria <gilbertoef@gmail.com>
2022-08-18 17:41:15 -0500

    Remove links to unnecessary files

--- locale-converter/src/App.jsx
+++ locale-converter/src/App.jsx
@@ -10,12 +10,12 @@
 import './App.css';
 import { useState } from 'react';
 import Papa from 'papaparse';
-import DataTable from './Components/DataTable'
-import { format_line } from './Modules/conversions'
+import DataTable from './components/DataTable'
+import { format_line } from './modules/conversions'
 
 function App() {
   // Storage for the CSV data
-  const [parseData, setParseData] = useState([]);
+  //const [parseData, setParseData] = useState([]);
   // Storage for the table headers
   const [tableRows, setTableRows] = useState([]);
   // Storage for values
@@ -24,9 +24,11 @@ function App() {
   const [newValues, setNewValues] = useState([]);
 
   const changeHandler = function(event) {
+    // Read the contents of a CSV file
     Papa.parse(event.target.files[0], {
       header: true,
       skipEmptyLines: true,
+      // Function to process the data read
       complete: (results) => {
         const rowsArray = [];
         const valuesArray = [];
@@ -37,10 +39,13 @@ function App() {
           rowsArray.push(Object.keys(item));
           valuesArray.push(Object.values(item));
           newValuesArray.push(format_line(Object.values(item)));
+          // Return a null to silence a warning.
+          // The map result is not used in any case
+          return null;
         });
 
         // Store the values in the state
-        setParseData(results.data);
+        //setParseData(results.data);
         setTableRows(rowsArray[0]);
         setValues(valuesArray);
         setNewValues(newValuesArray);
@@ -58,17 +63,18 @@ function App() {
           name="file"
           accept=".csv"
           onChange={changeHandler}
+          data-testid='input'
         />
       </div>
-      <table className="Data-table">
+      <table className="Data-table" data-testid='contents'>
         <tbody>
           <tr>
-            <td>
+            <td data-testid='original'>
               <h2>Original</h2>
               {/* Contents table */}
               <DataTable tableRows={tableRows} values={values} />
             </td>
-            <td>
+            <td data-testid='result'>
               <h2>Result</h2>
               {/* Contents table */}
               <DataTable tableRows={tableRows} values={newValues} />
