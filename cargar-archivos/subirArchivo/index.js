57136aba83daf5dc6f9fa54b2e0f9109cebfc024
Gilberto Echeverria <gilbertoef@gmail.com>
2023-05-24 11:27:52 -0600

    Update package files

--- cargar-archivos/subirArchivo/index.js
+++ cargar-archivos/subirArchivo/index.js
@@ -1,37 +1,35 @@
 const express = require("express")
 const multer  = require('multer')
 const bodyParser = require('body-parser');
-const uploads = multer({ dest: '.temp/' })
-const app=express();
 const fs = require('fs');
 const crypto = require('crypto');
 const { MongoClient } = require('mongodb');
 
+const app = express();
+const uploads = multer({ dest: '.temp/' })
+const port = 5000;
+
 app.use(bodyParser.urlencoded({ extended: true }))
-app.get("/upload", (req, res)=>{
-    res.render("upload.ejs")
-})
-let db;
 app.set('view engine', 'ejs')
 
-async function connectToDb() {
-    const client = new MongoClient('mongodb://localhost/mybooks');
-    await client.connect();
-    console.log('Connected to MongoDB');
-    db = client.db();
-}
+app.get("/upload", (req, res) => {
+    res.render("upload.ejs")
+})
 
-
-app.post("/upload", uploads.single("myFile"), (req, res)=>{
-    console.log(req.file.filename, req.body.nombre);
-    let inputFS=fs.createReadStream(__dirname+"/.temp/"+req.file.filename);
-    let outputFS=fs.createWriteStream(__dirname+"/.storage/"+req.body.nombre);
-    let key="abcabcabcabcabcabcabcabcabcabc12"
-    let iv="abcabcabcabcabc1"
-    let cipher=crypto.createCipheriv("aes-256-cbc", key, iv)
+app.post("/upload", uploads.single("myFile"), (req, res) => {
+    console.log("UPLOAD request: ", req.file.filename, req.body.nombre);
+    let tempFile = __dirname + "/.temp/" + req.file.filename;
+    let storeFile = __dirname + "/.storage/" + req.body.nombre;
+    console.log("tempFile: " + tempFile);
+    console.log("storeFile: " + storeFile);
+    let inputFS = fs.createReadStream(tempFile);
+    let outputFS = fs.createWriteStream(storeFile);
+    let key = "abcabcabcabcabcabcabcabcabcabc12"
+    let iv = "abcabcabcabcabc1"
+    let cipher = crypto.createCipheriv("aes-256-cbc", key, iv)
     inputFS.pipe(cipher).pipe(outputFS);
     outputFS.on('finish', function() {
-        fs.unlink(__dirname+"/.temp/"+req.file.filename, (err)=>{
+        fs.unlink(tempFile, (err) => {
             if (err) throw err;
             console.log("guardado archivo y borrado subido")
         })
@@ -39,38 +37,34 @@ app.post("/upload", uploads.single("myFile"), (req, res)=>{
     res.render("upload.ejs")
 })
 
-
-app.get("/descargar", (req, res)=>{
-    fs.readdir(__dirname+"/.storage", (err, files)=>{
+app.get("/descargar", (req, res) => {
+    fs.readdir(__dirname + "/.storage", (err, files) => {
         if (err) throw err;
         res.render("archivos.ejs", {archivos: files})
     });
-    
 })
 
-app.post("/descargar", (req, res)=>{
-    let archivoTemporal=__dirname+"/.temp/"+req.body.documentos+".pdf";
-    const inputFS=fs.createReadStream(__dirname+"/.storage/"+req.body.documentos);
-    const outputFS=fs.createWriteStream(archivoTemporal);
-    const key="abcabcabcabcabcabcabcabcabcabc12"
-    const iv="abcabcabcabcabc1"
-    const decipher=crypto.createDecipheriv("aes-256-cbc", key, iv)
+app.post("/descargar", (req, res) => {
+    let archivoTemporal = __dirname + "/.temp/" + req.body.documentos + ".pdf";
+    let storeFile = __dirname + "/.storage/" + req.body.documentos;
+    const inputFS = fs.createReadStream(storeFile);
+    const outputFS = fs.createWriteStream(archivoTemporal);
+    const key = "abcabcabcabcabcabcabcabcabcabc12"
+    const iv = "abcabcabcabcabc1"
+    const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv)
     inputFS.pipe(decipher).pipe(outputFS);
     outputFS.on('finish', function() {
-        res.download(archivoTemporal, (err)=>{
+        res.download(archivoTemporal, (err) => {
             if (err) throw err;
-            fs.unlink(archivoTemporal, (err)=>{
+            fs.unlink(archivoTemporal, (err) => {
                 if (err) throw err;
                 console.log("Borrado archivo despues de descargar")
             })
         })
-       console.log("creado el archivo a abrir"); 
+       console.log("creado el archivo a abrir");
     });
-    
 })
 
-app.listen(3000,() =>{
-    connectToDb();
-    console.log('Server started on port 3000...');
-  
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
 });
